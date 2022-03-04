7233b2914c7fb810f8e758de1f7b4dc2e30c4310
Peter Khalil <uni.03c9@gmail.com>
2022-03-04 22:23:12 +0200

    adding header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
+  <the-header></the-header>
   <router-view></router-view>
 </template>
 
 <script>
+import TheHeader from "./components/layout/TheHeader.vue";
 
+export default {
+  components: { TheHeader },
+};
 </script>
 
 <style>
