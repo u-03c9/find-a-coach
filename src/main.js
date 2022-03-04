52dc56feb6a5d85297e9dc6705c403824a0dbe2b
Peter Khalil <uni.03c9@gmail.com>
2022-03-04 23:18:45 +0200

    custom bases [card, button, badge]

--- src/main.js
+++ src/main.js
@@ -5,9 +5,17 @@ import store from "./store/index";
 
 import App from "./App.vue";
 
+import BaseBadge from "./components/ui/BaseBadge.vue";
+import BaseButton from "./components/ui/BaseButton.vue";
+import BaseCard from "./components/ui/BaseCard.vue";
+
 const vm = createApp(App);
 
 vm.use(router);
 vm.use(store);
+
+vm.component("base-card", BaseCard);
+vm.component("base-button", BaseButton);
+vm.component("base-badge", BaseBadge);
 
 vm.mount("#app");
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,25 +1,27 @@
 <template>
   <section>Filter</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link :to="{ name: 'register-coach' }">
-        Register as Coach
-      </router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :areas="coach.areas"
-        :rate="coach.hourlyRate"
-      >
-      </coach-item>
-    </ul>
-    <p v-else>No coaches found.</p>
+    <base-card>
+      <div class="controls">
+        <base-button>Refresh</base-button>
+        <base-button link to="/register">
+          Register as Coach
+        </base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :areas="coach.areas"
+          :rate="coach.hourlyRate"
+        >
+        </coach-item>
+      </ul>
+      <p v-else>No coaches found.</p>
+    </base-card>
   </section>
 </template>
 
