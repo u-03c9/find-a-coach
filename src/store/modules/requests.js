8eba1b468161873dd070e2cb52b88afc4298492b
Peter Khalil <uni.03c9@gmail.com>
2022-03-05 21:05:22 +0200

    filter incomming requests for the active coach only

--- src/store/modules/requests.js
+++ src/store/modules/requests.js
@@ -6,11 +6,12 @@ export default {
     };
   },
   getters: {
-    requests(state) {
-      return state.requests;
+    requests(state, _getters, _rootState, rootGetters) {
+      const coachId = rootGetters.userId;
+      return state.requests.filter((req) => req.coachId === coachId);
     },
-    hasRequests(state) {
-      return state.requests && state.requests.length > 0;
+    hasRequests(_state, getters) {
+      return getters.requests && getters.requests.length > 0;
     },
   },
   mutations: {
