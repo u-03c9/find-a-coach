2efd41cd7855d2108340cb99c5897e6f69b3ed4c
Peter Khalil <uni.03c9@gmail.com>
2022-03-06 01:19:08 +0200

    adding auth page

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import CoachRegister from "./pages/coaches/CoachRegistration.vue";
 import ContactCoach from "./pages/requests/ContactCoach.vue";
 import RequestsReceived from "./pages/requests/RequestsReceived.vue";
 import NotFound from "./pages/NotFound.vue";
+import UserAuth from "./pages/auth/UserAuth.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -23,6 +24,7 @@ const router = createRouter({
     },
     { name: "register-coach", path: "/register", component: CoachRegister },
     { name: "requests", path: "/requests", component: RequestsReceived },
+    { name: "auth", path: "/auth", component: UserAuth },
     { name: "not-found", path: "/:notFound(.*)", component: NotFound },
   ],
 });
